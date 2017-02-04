7813f4d2ddc40d38d38475f0d6231268682167bb
Gale Shafer <gshafer@squaremouth.com>
2017-02-04 13:11:55 -0500

    Add Admin Words Controller
    This commit adds the very basic pieces of an admin namespace and a words
    controller to start managing the addition of words.

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,4 @@
+class Admin::WordsController < AdminController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'maps#new'
+
+  namespace :admin do
+    root to: 'words#index'
+  end
 end
