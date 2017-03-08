37190adc05e6a71158d02486837e4b8670abd5a1
Gale Shafer <gshafer@squaremouth.com>
2017-03-08 18:09:48 -0500

    Test that tiles always returns an array
    We want tiles to always return an array because it's how the client will
    consume the JSON but the JSON serializer returns nil if there is nothing
    saved or if you try to save an empty object to the JSON column it
    instead saves it as NULL.

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Map, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:map) { Map.new }
+
+  describe "#tiles" do
+    it "returns an empty array when there are no tiles" do
+      expect(map.tiles).to eql []
+    end
+  end
 end
